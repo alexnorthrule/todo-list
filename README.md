f2bba197a94f1e956769fe4eb0c2dd98e0b52422
alexnorthrule <43405640+alexnorthrule@users.noreply.github.com>
2021-10-23 21:03:36 +0100

    First run through of the tutorial.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# todo-list
-Following a Youtube Tutorial for a Todo List
+# book-list
+Following a Youtube Tutorial for a Book List
+
+Full Credit goes to Traversey Media:
+
+https://www.youtube.com/watch?v=JaMCxVWtW58
+
+
---
+++ app.js
@@ -0,0 +1,154 @@
+// Bood Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+    static displayBooks() {
+            const books = Store.getBooks();
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href='#' class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+        
+    }
+
+    static deleteBook(el) {
+        if(el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        // Make Vanish in Three Seconds
+        setInterval( ()=> document.querySelector('.alert').remove(), 3000);
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+
+    }
+}
+
+// Store Class: Handles Storage
+
+class Store {
+
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+        
+        localStorage.setItem('books', JSON.stringify(books));
+
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if(book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+// Event: Add A Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    // Prevent Actual Submit
+    e.preventDefault();
+    // Get Form Values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+
+    // Validate Form
+
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please Fill In All Fields', 'danger');
+    } else {
+            //Instantiate Book
+
+            const book = new Book(title, author, isbn);
+
+            // Add Book to List
+
+
+            UI.addBookToList(book);
+
+            // Add book to store
+
+            Store.addBook(book);
+
+            // Show Success Message
+            UI.showAlert('Book added successfully', 'success');
+            // Clear Fields
+
+            UI.clearFields();
+
+    }
+
+
+})
+
+// Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+
+    // Remove book from UI
+    UI.deleteBook(e.target);
+
+    // Alert the User
+
+    UI.showAlert('Book removed', 'info');
+    // Remove from Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+
+});
+
+
